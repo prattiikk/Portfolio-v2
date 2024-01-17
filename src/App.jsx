a90a41867763271c0bb460d4885db3b92ae7355d
prattiikk <pvks5423@gmail.com>
2024-01-17 22:40:36 +0530

    added a basic loader to the whole website

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import "./App.css";
 import Home from "./Pages/Home";
+import Loading from "./Components/Loading";
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(
@@ -21,6 +22,24 @@ function App() {
   useEffect(() => {
     // localStorage.setIktem('isDarkMode', isDarkMode);
   }, [isDarkMode]);
+
+  const [assetsLoaded, setAssetsLoaded] = useState(false);
+
+  useEffect(() => {
+    // Simulate loading time
+    const loadingTimeout = setTimeout(() => {
+      // Set assetsLoaded to true after loading
+      setAssetsLoaded(true);
+    }, 2000); // Adjust the timeout based on your actual loading time
+
+    // Cleanup timeout to avoid memory leaks
+    return () => clearTimeout(loadingTimeout);
+  }, []);
+
+  // Render loading screen while assets are loading
+  if (!assetsLoaded) {
+    return <Loading />;
+  }
 
   return (
     <div className={`${isDarkMode ? " bg-black" : "bg-white"}`}>
---
+++ src/Components/Loading.jsx
@@ -0,0 +1,10 @@
+// Loading.js
+import React from "react";
+
+const Loading = () => (
+  <div className="flex items-center justify-center h-screen">
+    <span className="loading loading-infinity loading-lg"></span>
+  </div>
+);
+
+export default Loading;
